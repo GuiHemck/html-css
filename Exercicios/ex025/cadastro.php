d0edbc0984f790334f38b852a92f2cba26327e98
guihemck <146779002+GuiHemck@users.noreply.github.com>
2024-02-13 11:16:35 -0300

    ok

---
+++ Exercicios/ex025/cadastro.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+$nome= $_POST["iusu"];
+$email= $_POST["isenha"];
+
+
+echo "Seja bem vindo $nome seu email é $email"
+?>
+</body>
+</html>
